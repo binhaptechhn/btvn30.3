e37f82c5b675226b7c04fe2a666b0194ab3f368b
Le Thanh Binh (Aptech HN) <tbinh1799@gmail.com>
2022-03-31 21:26:29 +0700

    btvn30.3

---
+++ bt1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+float GetArena(float a, float b, float c);
+void main(int argc, const char * argv[]) {
+    float a,b,c,s;
+    printf("Enter numbers: ");
+    scanf("%f%f%f", &a, &b, &c);
+    if((a+b)>c && (a+c)>b && (b+c)>a)
+    {
+        s = GetArena(a, b, c);
+        printf("S: %.2f\n",s);
+         
+    }else
+    {
+        printf("Erros\n");
+    }
+}
+	float GetArena(float a, float b, float c){
+    float p,s;
+    p=(a+b+c)/2;
+    s = sqrt(p*(p-a)*(p-b)*(p-c));
+    return p;
+}
+
